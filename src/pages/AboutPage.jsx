e1e3d419a40e9b6aa7acd4b37bae85cbe92faa3d
iqolll <muhammadhaiqal1802@gmai.com>
2023-06-10 09:53:21 +0700

    Add router and about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,4 +1,5 @@
 import Card from '../components/shared/Card';
+import { Link } from 'react-router-dom';
 
 function AboutPage() {
   return (
@@ -7,6 +8,10 @@ function AboutPage() {
         <h1>About This Project</h1>
         <p>This is a React app to leave feedback for a product or service</p>
         <p>Version: 1.0.0</p>
+
+        <p>
+          <Link to="/">Back to home</Link>
+        </p>
       </div>
     </Card>
   );
